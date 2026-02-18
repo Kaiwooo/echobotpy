7186be397136025d67a349354ebb322f31f31729
p.gerasimenko <p.gerasimenko@medcall.center>
2026-02-18 14:45:44 +0300

    1

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 BITRIX_REST_URL = os.getenv("BITRIX_REST_URL")  # https://b24-.../rest/1/.../
-CONNECTOR_ID = os.getenv("BITRIX_CONNECTOR_ID")  # id вашего Connector API
+CONNECTOR_ID = os.getenv("CONNECTOR_ID")  # id вашего Connector API
 BITRIX_CLIENT_ID = os.getenv("BITRIX_CLIENT_ID")
 BITRIX_CLIENT_SECRET = os.getenv("BITRIX_CLIENT_SECRET")
